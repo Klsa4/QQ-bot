2b6a17925dad20accc68fc1fce46a1128cb84078
KariCat <1735016784@qq.com>
2024-04-15 23:54:02 +0800

    feat: 开发bot心跳检测

--- src/main/java/com/klsa/bot/websocket/BotSocketClient.java
+++ src/main/java/com/klsa/bot/websocket/BotSocketClient.java
@@ -2,6 +2,8 @@ package com.klsa.bot.websocket;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.LockSupport;
 
 import com.klsa.bot.pojo.Heartbeat;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,21 +41,21 @@ public class BotSocketClient {
     @Autowired
     GetAccessBotService getAccessBotService;
 
- 
+
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("WebSocket 连接已经建立。");
         this.session = session;
 
     }
- 
+
     @OnMessage
     public void onMessage(String json, Session session) {
         System.out.println("收到服务器消息：" + json);
         Payload payload = JSON.parseObject(json, Payload.class);
 
 
-        if(payload.getOp().equals(OpConstants.HELLO)) {
+        if (payload.getOp().equals(OpConstants.HELLO)) {
             Heartbeat hb = JSON.parseObject(payload.getD().toString(), Heartbeat.class);
             System.out.println(hb);
             this.interval = hb.getHeartbeat_interval();
@@ -69,7 +71,7 @@ public class BotSocketClient {
 
             BotToken botToken = getAccessBotService.getAccessToken(5);
 
-            if(botToken == null) {
+            if (botToken == null) {
                 log.info("token获取失败");
                 return;
             }
@@ -85,8 +87,8 @@ public class BotSocketClient {
             String response = JSON.toJSONString(feedback);
             System.out.println(response);
             sendMessage(response);
-        } else if(payload.getOp().equals(OpConstants.DISPATCH)) {
-            
+        } else if (payload.getOp().equals(OpConstants.DISPATCH)) {
+
         }
     }
 
@@ -98,41 +100,51 @@ public class BotSocketClient {
         }
     }
 
-    private  void heartbeat() {
-        while(true) {
-            try {
-                Payload payload = new Payload();
-                payload.setOp(OpConstants.HEARTBEAT);
-                if (lastestSequence != null) payload.setS(lastestSequence);
-                String json = JSON.toJSONString(payload);
-                System.out.println(json);
-                this.sendMessage(json);
-                Thread.sleep(interval);
-            } catch (Exception e) {
-                throw new RuntimeException("心跳出错");
+    @SuppressWarnings("ALL")
+    private void heartbeat() {
+        Executors.newSingleThreadExecutor(
+                runnable -> {
+                    Thread thread = new Thread(runnable);
+                    thread.setName("heartbeat");
+                    thread.setDaemon(Boolean.TRUE);
+                    return thread;
+                }
+        ).execute(() -> {
+            while (true) {
+                try {
+                    Payload payload = new Payload();
+                    payload.setOp(OpConstants.HEARTBEAT);
+                    if (lastestSequence != null) payload.setS(lastestSequence);
+                    String json = JSON.toJSONString(payload);
+                    System.out.println(json);
+                    this.sendMessage(json);
+                    LockSupport.parkUntil(interval);
+                } catch (Throwable e) {
+                    log.error("心跳出错: {}", e.getMessage());
+                }
             }
-        }
+        });
     }
- 
+
     @OnClose
     public void onClose() {
         System.out.println("WebSocket 连接已经关闭。");
     }
- 
+
     @OnError
     public void onError(Throwable t) {
         System.out.println("WebSocket 连接出现错误：" + t.getMessage());
     }
- 
+
     public void connect(String url) throws Exception {
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         container.connectToServer(this, new URI(url));
     }
- 
+
     public void send(String message) throws IOException {
         session.getBasicRemote().sendText(message);
     }
- 
+
     public void close() throws IOException {
         session.close();
     }
